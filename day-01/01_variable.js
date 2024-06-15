95a8f51207b3612d1f2f65e70e401d1a9fafe829
HastagKc <chaudharykshittiz950@gmail.com>
2024-06-15 22:59:50 +0545

    variable in js

---
+++ day-01/01_variable.js
@@ -0,0 +1,23 @@
+// variable in js
+// In JavaScript, a variable is a symbolic name for a value. 
+//Variables are used to store and manipulate data during the execution of a program. 
+// They provide a way to label and reference data, making code more readable and flexible.
+
+
+var name = 'kshittiz'
+
+// let use for variable declaration but it can not be reinitialize in same scope 
+let message = "Hello, world!"; 
+
+// this is used to declare constant value is js
+const bio = "software Developer" 
+
+
+// try to change each value 
+name = 'Ram'
+message = "Namaste World"
+// bio = "Js developer" // this variable can't reasign because it is contant variable 
+
+
+console.table([name,message, bio])
+
